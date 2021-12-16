4b371271c5780c73e20a0571c513f276548e6efe
Fatih Doğan <fatih@fatihdogan.dev>
2021-12-16 12:18:19 +0300

    wip

--- Binary/Package.swift
+++ Binary/Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.5.0
 import PackageDescription
 let package = Package(
-    name: "RSwiftBinary",
+    name: "R.swift.SPM.Binary",
     platforms: [.macOS(.v10_11)],
     dependencies: [
         .package(url: "https://github.com/mac-cain13/R.swift", from: "6.1.0"),
