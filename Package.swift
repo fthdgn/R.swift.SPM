5d362e6d50d1795f20b424e2dbe3083681de5ea9
Fatih Doğan <fatih@fatihdogan.dev>
2021-12-16 12:02:37 +0300

    wip

--- Package.swift
+++ Package.swift
@@ -1,13 +1,19 @@
 // swift-tools-version:5.5.0
 import PackageDescription
+
 let package = Package(
-    name: "R.Swift.SPM",
-    platforms: [
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/mac-cain13/R.swift.Library", .upToNextMinor(from: "5.3.0"))
-    ]
+  name: "R.Swift.SPM",
+  platforms: [
+    .iOS(.v9),
+    .tvOS(.v9),
+    .watchOS(.v2),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/mac-cain13/R.swift.Library", .upToNextMinor(from: "5.3.0"))
+  ],
+  products: [
+    .library(
+      name: "R.Swift.SPM",
+      targets: ["R.Swift.SPM"])
+  ],
 )
